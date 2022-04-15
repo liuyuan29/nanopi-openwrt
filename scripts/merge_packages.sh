ee3ad528d39429f65fb68638b779a4de818995dc
liuyuan29 <58924895+liuyuan29@users.noreply.github.com>
2022-04-15 11:53:34 +0800

    Update merge_packages.sh

--- scripts/merge_packages.sh
+++ scripts/merge_packages.sh
@@ -29,11 +29,14 @@ rm -rf package/custom; mkdir package/custom
 merge_feed nas "https://github.com/linkease/nas-packages;master"
 merge_feed nas_luci "https://github.com/linkease/nas-packages-luci;main"
 merge_feed helloworld "https://github.com/fw876/helloworld;master"
-merge_package https://github.com/klever1988/helloworld/branches/lean/luci-app-ssr-plus
-merge_package https://github.com/klever1988/helloworld/branches/lean/mosdns
+#merge_package https://github.com/klever1988/helloworld/branches/lean/luci-app-ssr-plus
+#merge_package https://github.com/klever1988/helloworld/branches/lean/mosdns
 #merge_package https://github.com/klever1988/openwrt-mos/trunk/luci-app-mosdns
-merge_package https://github.com/sbwml/luci-app-mosdns
-merge_package https://github.com/rufengsuixing/luci-app-adguardhome
+merge_package https://github.com/kenzok8/jell/trunk/luci-app-adguardhome
+merge_package https://github.com/kenzok8/jell/trunk/mosdns
+merge_package https://github.com/kenzok8/jell/trunk/luci-app-mosdns
+#merge_package https://github.com/sbwml/luci-app-mosdns
+#merge_package https://github.com/rufengsuixing/luci-app-adguardhome
 merge_package https://github.com/project-lede/luci-app-godproxy
 merge_package https://github.com/sundaqiang/openwrt-packages/trunk/luci-app-wolplus
 merge_package https://github.com/kuoruan/openwrt-frp frp
