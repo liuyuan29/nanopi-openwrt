5ca4e3cc19e6107d45dc4f245184826e7b7ab44f
liuyuan29 <58924895+liuyuan29@users.noreply.github.com>
2022-05-14 20:59:43 +0800

    Update merge_packages.sh

--- scripts/merge_packages.sh
+++ scripts/merge_packages.sh
@@ -29,7 +29,7 @@ rm -rf package/custom; mkdir package/custom
 merge_feed nas "https://github.com/linkease/nas-packages;master"
 merge_feed nas_luci "https://github.com/linkease/nas-packages-luci;main"
 merge_feed helloworld "https://github.com/fw876/helloworld;master"
-merge_package https://github.com/klever1988/helloworld/branches/lean/luci-app-ssr-plus
+#merge_package https://github.com/klever1988/helloworld/branches/lean/luci-app-ssr-plus
 merge_package https://github.com/klever1988/helloworld/branches/lean/mosdns
 #merge_package https://github.com/klever1988/openwrt-mos/trunk/luci-app-mosdns
 #merge_package https://github.com/kenzok8/jell/trunk/luci-app-adguardhome
